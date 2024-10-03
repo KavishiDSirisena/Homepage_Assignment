ab3442b63ef7bbd9d79e4e974d1282bf14d2fca2
Kavishi Sirisena <kavishisirisena177@gmail.com>
2024-10-03 15:36:24 +0530

    Initializing the project

---
+++ home-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Register from './pages/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+     
+    
+
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
