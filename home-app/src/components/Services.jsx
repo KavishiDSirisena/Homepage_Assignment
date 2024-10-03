08e3c42aee7468bf388250ca278b48a21e18cf8d
Kavishi Sirisena <kavishisirisena177@gmail.com>
2024-10-04 01:49:25 +0530

    Adding Css to style services

--- home-app/src/components/Services.jsx
+++ home-app/src/components/Services.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
+import '../styles/styles.css';
 
 const Services = () => {
   return (
-    <section id="services" className="py-5">
+    <section id="services" className="py-5 services-section">
       <div className="container">
-        <h2 className="text-center">Our Services</h2>
+        <h2 className="text-center text-white">Our Services</h2>
         <div className="row">
           <div className="col-md-4">
-            <h4>Service 1</h4>
-            <p></p>
+            <div className="service-card">
+              <h4>Personal Styling</h4>
+              <p>Receive tailored fashion advice and outfit recommendations from our expert stylists to elevate your wardrobe.</p>
+              <img src={'Services.png'} alt="Logo" className="service-logo" />
+
+            </div>
           </div>
           <div className="col-md-4">
-            <h4>Service 2</h4>
-            <p></p>
+            <div className="service-card">
+              <h4>Custom Tailoring</h4>
+              <p>Get your clothing perfectly fitted with our custom tailoring services to ensure you look and feel your best.</p>
+              <img src={'Services.png'} alt="Logo" className="service-logo" />
+
+            </div>
           </div>
           <div className="col-md-4">
-            <h4>Service 3</h4>
-            <p></p>
+            <div className="service-card">
+              <h4>Fashion Consultations</h4>
+              <p>Join our exclusive fashion consultations to stay updated on the latest trends and styles that suit you.</p>
+              <img src={'Services.png'} alt="Logo" className="service-logo" />
+            </div>
           </div>
         </div>
       </div>
