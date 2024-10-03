cd9e252a1168c074b474b77847ebe6571c685904
Kavishi Sirisena <kavishisirisena177@gmail.com>
2024-10-03 16:25:42 +0530

    Completing Footer

---
+++ home-app/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-light py-4">
+      <div className="container">
+        <p className="text-center">© 2024 CompanyName. All Rights Reserved.</p>
+        <p className="text-center">
+          <a href="#privacy-policy">Privacy Policy</a> | <a href="#terms">Terms of Use</a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
